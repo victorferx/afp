05e5861f56d6df59f2a4a2a06c1adc4bfbeac315
Victor Fernández <victorferx@gmail.com>
2022-08-01 23:53:23 -0500

    Carga de archivos
    Carga de archivos

---
+++ gateway.service/src/test/java/com/nttdata/afp/gateway/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.afp.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
